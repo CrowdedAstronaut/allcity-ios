29c23ed97ee087b29edae082752e6550a9edf0e1
CrowdedAstronaut <ross@johnrossphillips.com>
2021-12-02 14:30:23 -0600

    Added .gif assets

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import GalleryView from "./screens/GalleryView";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Camera } from "expo-camera";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
